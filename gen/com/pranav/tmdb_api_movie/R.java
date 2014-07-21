c2f256cef3a4b3aab2798a189a80b190027a14e8
unknown <pranavrshah2012@gmail.com>
2014-07-21 03:00:38 -0500

    Modified detailed_view.xml

--- gen/com/pranav/tmdb_api_movie/R.java
+++ gen/com/pranav/tmdb_api_movie/R.java
@@ -1082,7 +1082,7 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020057;
     }
     public static final class id {
-        public static final int View_movies=0x7f050048;
+        public static final int View_movies=0x7f050049;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1094,11 +1094,12 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05004a;
+        public static final int action_settings=0x7f05004b;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
-        public static final int cast_details=0x7f050042;
+        public static final int button1=0x7f05003f;
+        public static final int cast_details=0x7f050043;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f05003c;
@@ -1116,15 +1117,15 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
-        public static final int image_of_movie=0x7f050040;
+        public static final int image_of_movie=0x7f050041;
         public static final int listMode=0x7f050001;
-        public static final int list_image=0x7f050044;
+        public static final int list_image=0x7f050045;
         public static final int list_item=0x7f050029;
-        public static final int menu_settings=0x7f050049;
+        public static final int menu_settings=0x7f05004a;
         public static final int middle=0x7f050012;
-        public static final int movie_details=0x7f050041;
-        public static final int movie_result_list_item=0x7f050047;
-        public static final int name_of_movie=0x7f05003f;
+        public static final int movie_details=0x7f050042;
+        public static final int movie_result_list_item=0x7f050048;
+        public static final int name_of_movie=0x7f050040;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
@@ -1132,7 +1133,7 @@ containing a value of this type.
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
-        public static final int rating=0x7f050045;
+        public static final int rating=0x7f050046;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1150,13 +1151,13 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int thumbnail=0x7f050043;
+        public static final int thumbnail=0x7f050044;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
         public static final int withText=0x7f05000c;
-        public static final int year=0x7f050046;
+        public static final int year=0x7f050047;
     }
     public static final class integer {
         /**  The maximum number of action buttons that should be permitted within
